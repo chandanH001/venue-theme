a4833a9b66c9eead664aa531e5b99a923012a792
Chandan <chandan.halder96@gmail.com>
2025-07-23 15:54:43 +0600

    venue theme

---
+++ header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Venue - Responsive Web Template</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php ?>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/fontAwesome.css">
+    <link rel="stylesheet" href="css/hero-slider.css">
+    <link rel="stylesheet" href="css/owl-carousel.css">
+    <link rel="stylesheet" href="css/datepicker.css">
+    <link rel="stylesheet" href="css/templatemo-style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <!--
+	Venue Template
+	http://www.templatemo.com/tm-522-venue
+-->
+</head>
+
+<body>
+
+    <div class="wrap">
+        <header id="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 flex justify-between">
+                        <button id="primary-nav-button" type="button">Menu</button>
+                        <a class="flex" href="index.html">
+                            <div class="logo">
+                                <img src="img/logo.png" alt="Venue Logo">
+                            </div>
+                        </a>
+                        <nav id="primary-nav" class="dropdown cf">
+                            <ul class="dropdown menu">
+                                <li class='active'><a href="#">Popular</a></li>
+                                <li><a href="#">Most Visited</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="#">Most Visited 1</a>
+                                        </li>
+                                        <li><a href="#">Most Visited 2</a>
+                                        </li>
+                                        <li><a href="#">Most Visited 3</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a class="scrollTo" data-scrollTo="blog" href="#">Blog Entries</a></li>
+                                <li><a class="scrollTo" data-scrollTo="services" href="#">Our Services</a></li>
+                                <li><a class="scrollTo" data-scrollTo="contact" href="#">Contact Us</a></li>
+                            </ul>
+                        </nav><!-- / #primary-nav -->
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
